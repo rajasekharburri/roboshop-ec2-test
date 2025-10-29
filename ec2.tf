71794e450bd4ded968392e15a192dfda6e2f2e1d
rajasekhar burri <burriraja@gmail.com>
2025-10-30 01:29:42 +0530

    terr

---
+++ ec2.tf
@@ -0,0 +1,27 @@
+module "catalogue" {
+    source = "../terraform-aws-instances"
+    # ami_id = "ami-09c813fb71547fc4f"
+    # sg_ids = ["sg-02baf6b4ad5d16f19"]
+    # instance_type = "t3.micro"
+    # tags = {
+    #     Name = "modu"
+    # }
+
+    ami_id = var.ami_id
+    sg_ids = var.sg_ids
+    instance_type = var.instance_type
+    tags = var.tags
+    
+}
+
+output "pub_ip" {
+    value = module.catalogue.public_ip
+}
+
+output "priv_ip" {
+    value = module.catalogue.private_ip
+}
+
+output "id" {
+    value = module.catalogue.instance_id
+}
